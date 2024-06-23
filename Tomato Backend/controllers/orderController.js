a956df517316ca0e7f0e4ff79b8e6a09a762de6d
bhavesh-s2002 <bhavesh.s14sept@gmail.com>
2024-06-23 12:41:18 +0530

    Created cart functionality using db

---
+++ Tomato Backend/controllers/orderController.js
@@ -0,0 +1,9 @@
+import orderModel from "../models/orderModel";
+import userModel from "../models/userModels.js"
+import stripe from "stripe"
+
+
+// Placing user order from frontend
+const placeOrder =async(req,res)=>{
+    
+}
